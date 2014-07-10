c1c511babea33804b898ad1ff57a85e58885e5b6
John <635253084@qq.com>
2014-07-10 17:35:58 +0800

    添加测试

---
+++ ChatApp/ServerPoint/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ServerPoint
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, 8000);
+			Socket newSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			newSocket.Bind(localEndPoint);
+
+			newSocket.Listen(10);
+			var client = newSocket.Accept();
+			Console.WriteLine("连接成功");
+		}
+	}
+}
---
+++ ChatApp/TcpClientPoint/TcpClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TcpClientPoint
+{
+	public class TcpClient
+	{
+		public TcpClient()
+		{
+			IPEndPoint ipend = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8000);
+			var server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			server.Connect(ipend);
+		}
+	}
+}
